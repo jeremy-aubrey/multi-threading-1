79d1f960f48e1525cc731218454c7852abdd2555
jeremy aubrey <jeremyaubrey@gmail.com>
2022-02-24 14:32:29 -0600

    implemented call method (returns minimum)

--- src/MinCallable.java
+++ src/MinCallable.java
@@ -1,11 +1,27 @@
+import java.util.List;
 import java.util.concurrent.Callable;
 
-public class MinCallable implements Callable<Integer>{
+public class MinCallable implements Callable<Integer> {
+	
+	private List<Integer> data;
+	
+	public MinCallable(List<Integer> data) {
+		this.data = data;
+	}
 
 	@Override
 	public Integer call() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		int min = Integer.MAX_VALUE;
+		if(!data.isEmpty()) {
+			for(Integer num : data) {
+				int currentInt = num.intValue();
+				if(currentInt < min) {
+					min = currentInt;
+				}
+			}
+		}
+		
+		return new Integer(min);
 	}
 
 }
